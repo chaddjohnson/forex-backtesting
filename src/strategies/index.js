cf5bc7d0194914794bf73ade6facbb0661dd588e
Chad Johnson <chad.d.johnson@gmail.com>
2015-08-27 17:54:29 -0500

    Added Random strategy to test win rate of playing randomly.

--- src/strategies/index.js
+++ src/strategies/index.js
@@ -1,2 +1,3 @@
 module.exports.Reversals = require('./reversals');
 module.exports.MorningTrend = require('./morningTrend');
+module.exports.Random = require('./random');
---
+++ src/strategies/random.js
@@ -0,0 +1,59 @@
+var studies = require('../studies');
+var Base = require('./base');
+var Call = require('../positions/call');
+var Put = require('../positions/put');
+
+// Define studies to use.
+var studyDefinitions = [];
+
+function Random() {
+    this.constructor = Random;
+    Base.call(this);
+
+    this.prepareStudies(studyDefinitions);
+}
+
+// Create a copy of the Base "class" prototype for use in this "class."
+Random.prototype = Object.create(Base.prototype);
+
+Random.prototype.backtest = function(data, investment, profitability) {
+    var self = this;
+    var waitTicks = 0;
+    var decisionPivot = 0;
+    var previousDataPoint;
+
+    // For every data point...
+    data.forEach(function(dataPoint, index) {
+        // Simulate the next tick, and process update studies for the tick.
+        self.tick(dataPoint);
+
+        if (waitTicks <= 0 && previousDataPoint) {
+            decisionPivot = Math.floor(Math.random() * ((100 - 1) + 1)) + 1;
+            waitTicks = Math.floor(Math.random() * ((250 - 1) + 1)) + 1;
+
+            if (decisionPivot <= 50) {
+                self.addPosition(new Call(dataPoint.symbol, dataPoint.timestamp, previousDataPoint.close, investment, profitability, 120));
+            }
+            else {
+                self.addPosition(new Put(dataPoint.symbol, dataPoint.timestamp, previousDataPoint.close, investment, profitability, 120));
+            }
+        }
+
+        waitTicks--;
+
+        // Track the current data point as the previous data point for the next tick.
+        previousDataPoint = dataPoint;
+    });
+
+    // Show the results.
+    console.log('SYMBOL:\t\t' + previousDataPoint.symbol);
+    console.log('PROFIT/LOSS:\t$' + self.getProfitLoss());
+    console.log('WIN RATE:\t' + self.getWinRate());
+    console.log('WINS:\t\t' + self.winCount);
+    console.log('LOSSES:\t\t' + self.loseCount);
+
+    // Save the output to a file.
+    this.saveOutput();
+};
+
+module.exports = Random;

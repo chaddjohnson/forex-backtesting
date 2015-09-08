52c63650a438d4cb4ff07c8058e1d5352f30a257
Chad Johnson <chad.d.johnson@gmail.com>
2015-09-08 00:39:10 -0500

    Added AverageVolume study.

---
+++ src/studies/averageVolume.js
@@ -0,0 +1,34 @@
+var Base = require('./base');
+var _ = require('underscore');
+
+function AverageVolume(inputs, outputMap) {
+    this.constructor = AverageVolume;
+    Base.call(this, inputs, outputMap);
+
+    if (!inputs.length) {
+        throw 'No length input parameter provided to study.';
+    }
+}
+
+// Create a copy of the Base "class" prototype for use in this "class."
+AverageVolume.prototype = Object.create(Base.prototype);
+
+AverageVolume.prototype.tick = function() {
+    var dataSegment = this.getDataSegment(this.getInput('length'));
+    var average = 0.0;
+    var returnValue = {};
+
+    if (dataSegment.length < this.getInput('length')) {
+        return '';
+    }
+
+    average = _(dataSegment).reduce(function(memo, dataPoint) {
+        return memo + dataPoint.volume;
+    }, 0) / dataSegment.length;
+
+    returnValue[this.getOutputMapping('average')] = average;
+
+    return returnValue;
+};
+
+module.exports = AverageVolume;
--- src/studies/index.js
+++ src/studies/index.js
@@ -2,3 +2,4 @@ module.exports.Ema = require('./ema');
 module.exports.Sma = require('./sma');
 module.exports.Rsi = require('./rsi');
 module.exports.PolynomialRegressionCurve = require('./polynomialRegressionCurve');
+module.exports.AverageVolume = require('./averageVolume');

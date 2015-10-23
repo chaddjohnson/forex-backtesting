77e2dc412a382e26c90a0d4c31f53da158553fd9
Chad Johnson <chad.d.johnson@gmail.com>
2015-10-23 17:02:29 -0500

    Sorting datapoints by data.timestamp when streaming.

--- src/optimizers/Base.js
+++ src/optimizers/Base.js
@@ -272,7 +272,7 @@ Base.prototype.optimize = function(configurations, investment, profitability, ca
             stream.resume();
         };
 
-        var stream = DataPoint.find({symbol: self.symbol}, {}, {timeout: true}).stream();
+        var stream = DataPoint.find({symbol: self.symbol}, {}, {timeout: true}).sort({'data.timestamp': 1}).stream();
 
         stream.on('data', streamer);
         stream.on('close', function() {

29d46ba315871419a517674c012df80cc5fc003d
Chad Johnson <chad.d.johnson@gmail.com>
2015-10-23 01:01:33 -0500

    Undid a change to optimize.sh.

--- optimize.sh
+++ optimize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node --nouse-idle-notification --max-old-space-size=31000 --expose-gc `which gulp` optimize --symbol $1 --parser metatrader --data ./data/metatrader/one-week/$1.csv --optimizer Reversals --investment 1000 --profitability 0.76 --database forex-backtesting
+node --nouse-idle-notification --max-old-space-size=31000 --expose-gc `which gulp` optimize --symbol $1 --parser metatrader --data ./data/metatrader/three-year/$1.csv --optimizer Reversals --investment 1000 --profitability 0.76 --database forex-backtesting

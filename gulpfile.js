9d5b7f3d73471c365e8ec3d5c3ba502185a53e27
Chad Johnson <chad.d.johnson@gmail.com>
2015-12-04 03:32:40 -0600

    Tightened up combination constraints.

--- gulpfile.js
+++ gulpfile.js
@@ -199,7 +199,8 @@ gulp.task('combine', function(done) {
         minimumProfitLoss: {'$gte': -10000},
         maximumConsecutiveLosses: {'$lte': 10},
         winRate: {'$gte': 0.65},
-        profitLoss: {'$gte': 30000}
+        profitLoss: {'$gte': 30000},
+        tradeCount: {'$gte': 425}
     };
 
     // Find the symbol based on the command line argument.
@@ -267,7 +268,7 @@ gulp.task('combine', function(done) {
                     var testResults = positionTester.test(testPositions);
 
                     // See if the test resulted in an improvement.
-                    if (testResults.profitLoss >= benchmarkProfitLoss + 1000 && testResults.winRate >= 0.65 && testResults.profitLoss >= 30000 && testResults.maximumConsecutiveLosses <= 10 && testResults.minimumProfitLoss >= -10000) {
+                    if (testResults.profitLoss >= benchmarkProfitLoss + 1000 && testResults.winRate >= 0.65 && testResults.profitLoss >= 30000 && testResults.tradeCount >= 425 && testResults.maximumConsecutiveLosses <= 10 && testResults.minimumProfitLoss >= -10000) {
                         // Use the positions in future tests.
                         optimalPositions = testPositions;
 

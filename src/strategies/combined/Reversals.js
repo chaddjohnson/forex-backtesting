196a5fa8fbbc7580779ff3fe32c7edd842b21c3e
Chad Johnson <chad.d.johnson@gmail.com>
2016-02-16 06:07:58 -0600

    Made Reversals strategy more efficient.

--- src/strategies/combined/Reversals.js
+++ src/strategies/combined/Reversals.js
@@ -62,6 +62,57 @@ ReversalsCombined.prototype.backtest = function(data, investment, profitability)
             putThisConfiguration = true;
             callThisConfiguration = true;
 
+            if (configuration.prChannel) {
+                if (dataPoint[configuration.prChannel.upper] && dataPoint[configuration.prChannel.lower]) {
+                    // Determine if the upper regression bound was not breached by the high price.
+                    if (putThisConfiguration && (!dataPoint[configuration.prChannel.upper] || dataPoint.high <= dataPoint[configuration.prChannel.upper])) {
+                        putThisConfiguration = false;
+                    }
+
+                    // Determine if the lower regression bound was not breached by the low price.
+                    if (callThisConfiguration && (!dataPoint[configuration.prChannel.lower] || dataPoint.low >= dataPoint[configuration.prChannel.lower])) {
+                        callThisConfiguration = false;
+                    }
+                }
+                else {
+                    putThisConfiguration = false;
+                    callThisConfiguration = false;
+                }
+            }
+            if (configuration.stochastic) {
+                if (typeof dataPoint[configuration.stochastic.K] === 'number' && typeof dataPoint[configuration.stochastic.D] === 'number') {
+                    // Determine if stochastic is not above the overbought line.
+                    if (putThisConfiguration && (dataPoint[configuration.stochastic.K] <= configuration.stochastic.overbought || dataPoint[configuration.stochastic.D] <= configuration.stochastic.overbought)) {
+                        putThisConfiguration = false;
+                    }
+
+                    // Determine if stochastic is not below the oversold line.
+                    if (callThisConfiguration && (dataPoint[configuration.stochastic.K] >= configuration.stochastic.oversold || dataPoint[configuration.stochastic.D] >= configuration.stochastic.oversold)) {
+                        callThisConfiguration = false;
+                    }
+                }
+                else {
+                    putThisConfiguration = false;
+                    callThisConfiguration = false;
+                }
+            }
+            if (configuration.rsi) {
+                if (typeof dataPoint[configuration.rsi.rsi] === 'number') {
+                    // Determine if RSI is not above the overbought line.
+                    if (putThisConfiguration && dataPoint[configuration.rsi.rsi] <= configuration.rsi.overbought) {
+                        putThisConfiguration = false;
+                    }
+
+                    // Determine if RSI is not below the oversold line.
+                    if (callThisConfiguration && dataPoint[configuration.rsi.rsi] >= configuration.rsi.oversold) {
+                        callThisConfiguration = false;
+                    }
+                }
+                else {
+                    putThisConfiguration = false;
+                    callThisConfiguration = false;
+                }
+            }
             if (configuration.ema200 && configuration.ema100) {
                 if (!dataPoint.ema200 || !dataPoint.ema100) {
                     putThisConfiguration = false;
@@ -107,57 +158,6 @@ ReversalsCombined.prototype.backtest = function(data, investment, profitability)
 
                 // Determine if an uptrend is not occurring.
                 if (callThisConfiguration && dataPoint.ema50 > dataPoint.sma13) {
-                    callThisConfiguration = false;
-                }
-            }
-            if (configuration.rsi) {
-                if (typeof dataPoint[configuration.rsi.rsi] === 'number') {
-                    // Determine if RSI is not above the overbought line.
-                    if (putThisConfiguration && dataPoint[configuration.rsi.rsi] <= configuration.rsi.overbought) {
-                        putThisConfiguration = false;
-                    }
-
-                    // Determine if RSI is not below the oversold line.
-                    if (callThisConfiguration && dataPoint[configuration.rsi.rsi] >= configuration.rsi.oversold) {
-                        callThisConfiguration = false;
-                    }
-                }
-                else {
-                    putThisConfiguration = false;
-                    callThisConfiguration = false;
-                }
-            }
-            if (configuration.stochastic) {
-                if (typeof dataPoint[configuration.stochastic.K] === 'number' && typeof dataPoint[configuration.stochastic.D] === 'number') {
-                    // Determine if stochastic is not above the overbought line.
-                    if (putThisConfiguration && (dataPoint[configuration.stochastic.K] <= configuration.stochastic.overbought || dataPoint[configuration.stochastic.D] <= configuration.stochastic.overbought)) {
-                        putThisConfiguration = false;
-                    }
-
-                    // Determine if stochastic is not below the oversold line.
-                    if (callThisConfiguration && (dataPoint[configuration.stochastic.K] >= configuration.stochastic.oversold || dataPoint[configuration.stochastic.D] >= configuration.stochastic.oversold)) {
-                        callThisConfiguration = false;
-                    }
-                }
-                else {
-                    putThisConfiguration = false;
-                    callThisConfiguration = false;
-                }
-            }
-            if (configuration.prChannel) {
-                if (dataPoint[configuration.prChannel.upper] && dataPoint[configuration.prChannel.lower]) {
-                    // Determine if the upper regression bound was not breached by the high price.
-                    if (putThisConfiguration && (!dataPoint[configuration.prChannel.upper] || dataPoint.high <= dataPoint[configuration.prChannel.upper])) {
-                        putThisConfiguration = false;
-                    }
-
-                    // Determine if the lower regression bound was not breached by the low price.
-                    if (callThisConfiguration && (!dataPoint[configuration.prChannel.lower] || dataPoint.low >= dataPoint[configuration.prChannel.lower])) {
-                        callThisConfiguration = false;
-                    }
-                }
-                else {
-                    putThisConfiguration = false;
                     callThisConfiguration = false;
                 }
             }

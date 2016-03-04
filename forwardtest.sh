b9a4e5f0ef637c206c2c5b1f4992f8de9f3f42e9
Chad Johnson <chad.d.johnson@gmail.com>
2016-03-04 17:02:09 -0600

    Changed paths to backtest and forwardtest files.

--- forwardtest.sh
+++ forwardtest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node --nouse-idle-notification --max-old-space-size=31000 --expose-gc `which gulp` forwardtest --symbol $1 --parser ctoption --data ./data/ctoption/$1.csv --investment 1000 --profitability 0.76 --database forex-backtesting
+node --nouse-idle-notification --max-old-space-size=31000 --expose-gc `which gulp` forwardtest --symbol $1 --parser metatrader --data ./data/metatrader/forwardtest/$1.csv --investment 1000 --profitability 0.76 --database forex-backtesting

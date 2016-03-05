102cb5f0631e2e5d13b98b005e153e2eceec02cf
Chad Johnson <chad.d.johnson@gmail.com>
2016-03-05 02:15:04 -0600

    Added missing fields to Validation model

--- src/models/Validation.js
+++ src/models/Validation.js
@@ -2,6 +2,9 @@ var mongoose = require('mongoose');
 
 var validationSchema = mongoose.Schema({
     symbol: {type: String, required: true},
+    round: {type: Number, required: true},
+    strategyUuid: {type: String, required: true},
+    configuration: {type: mongoose.Schema.Types.Mixed, required: true},
     profitLoss: {type: Number, required: true},
     winCount: {type: Number, required: true},
     loseCount: {type: Number, required: true},

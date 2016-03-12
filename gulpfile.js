87cc3da2f0ea2eaccf962a95ce2eb8ac067a4584
Chad Johnson <chad.d.johnson@gmail.com>
2016-03-11 18:22:24 -0600

    Added "average" task.

--- gulpfile.js
+++ gulpfile.js
@@ -206,30 +206,102 @@ gulp.task('test', function(done) {
 });
 
 gulp.task('average', function(done) {
-    // var Forwardtest = require('./src/models/Forwardtest');
-    // var ValidationAverage = require('./src/models/ValidationAverage');
+    function showUsageInfo() {
+        console.log('Example usage:\n');
+        console.log('gulp average --symbol AUDJPY --database forex-backtesting\n');
+    }
 
-    // // Find the forward tests for group 10.
-    // Forwardtest.find({...}, function(error, forwardtests) {
-    //     // For each forward test, find the validation results for each round given the forward test's configuration.
-    //     Validation.find({...}, function(error, validations) {
-    //         // Calculate averages for all properties.
-    //         var averageProfitLoss = _.reduce(...);
-    //         var averageTradeCount = _.reduce(...);
-    //         var averageWinRate = _.reduce(...);
-    //         var averageMaximumConsecutiveLosses = _.reduce(...);
-    //         var averageMinimumProfitLoss = _.reduce(...);
+    function handleInputError(message) {
+        gutil.log(gutil.colors.red(message));
+        showUsageInfo();
+        process.exit(1);
+    }
 
-    //         // Save averages.
-    //         ValidationAverage.create({
-    //             symbol: argv.symbol,
-    //             configuration: forwardtest.configuration,
-    //             profitLoss: averageProfitLoss,
-    //             tradeCount: averageTradeCount,
-    //             winRate: averageWinRate,
-    //             maximumConsecutiveLosses: averageMaximumConsecutiveLosses,
-    //             minimumProfitLoss: averageMinimumProfitLoss
-    //         });
-    //     });
-    // });
+    var db = require('./db');
+    var Forwardtest = require('./src/models/Forwardtest');
+    var Validation = require('./src/models/Validation');
+    var ValidationAverage = require('./src/models/ValidationAverage');
+
+    // Find the symbol based on the command line argument.
+    if (!argv.symbol) {
+        handleInputError('No symbol provided');
+    }
+
+    if (!argv.database) {
+        handleInputError('No database provided');
+    }
+
+    // Set up database connection.
+    db.initialize(argv.database);
+
+    // Find the forward tests for group 10.
+    Forwardtest.find({symbol: argv.symbol, group: 10}, function(error, forwardtests) {
+        var tasks = [];
+        var forwardtestCount = forwardtests.length;
+        var percentage = 0.0;
+        var validationAverages = [];
+
+        process.stdout.write('Calculating validation averages...');
+
+        forwardtests.forEach(function(forwardtest, index) {
+            tasks.push(function(taskCallback) {
+                // For each forward test, find the validation results for each round given the forward test's configuration.
+                Validation.find({configuration: forwardtest.configuration}, function(error, validations) {
+                    var validationCount = validations.length;
+
+                    // Calculate averages for all properties.
+                    var averageProfitLoss = _.reduce(validations, function(memo, validation) {
+                        return memo + validation.profitLoss;
+                    }, 0) / validationCount;
+
+                    var averageTradeCount = _.reduce(validations, function(memo, validation) {
+                        return memo + validation.tradeCount;
+                    }, 0) / validationCount;
+
+                    var averageWinRate = _.reduce(validations, function(memo, validation) {
+                        return memo + validation.winRate;
+                    }, 0) / validationCount;
+
+                    var averageMaximumConsecutiveLosses = _.reduce(validations, function(memo, validation) {
+                        return memo + validation.maximumConsecutiveLosses;
+                    }, 0) / validationCount;
+
+                    var averageMinimumProfitLoss = _.reduce(validations, function(memo, validation) {
+                        return memo + validation.minimumProfitLoss;
+                    }, 0) / validationCount;
+
+                    // Add to averages to be saved.
+                    validationAverages.push({
+                        symbol: argv.symbol,
+                        configuration: forwardtest.configuration,
+                        profitLoss: averageProfitLoss,
+                        tradeCount: averageTradeCount,
+                        winRate: averageWinRate,
+                        maximumConsecutiveLosses: averageMaximumConsecutiveLosses,
+                        minimumProfitLoss: averageMinimumProfitLoss
+                    });
+
+                    percentage = ((index / forwardtestCount) * 100).toFixed(5);
+                    process.stdout.cursorTo(34);
+                    process.stdout.write(percentage + '%');
+
+                    taskCallback();
+                });
+            });
+        });
+
+        async.series(tasks, function() {
+            process.stdout.cursorTo(34);
+            process.stdout.write((100).toFixed(5) + '%\n');
+            process.stdout.write('Saving validation averages...');
+
+            ValidationAverage.collection.insert(validationAverages, function(error) {
+                process.stdout.write('done\n');
+
+                db.disconnect();
+                done();
+                process.exit(0);
+            });
+        });
+    });
 });

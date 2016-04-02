3d64a4d5bee929b492d73210b411e7813d0fe102
Chad Johnson <chad.d.johnson@gmail.com>
2016-04-02 15:09:43 -0500

    Added oanda data parser.

--- src/dataParsers/index.js
+++ src/dataParsers/index.js
@@ -1,3 +1,4 @@
 module.exports.dukascopy = require('./dukascopy');
 module.exports.metatrader = require('./metatrader');
 module.exports.ctoption = require('./ctoption');
+module.exports.oanda = require('./oanda');
---
+++ src/dataParsers/oanda.js
@@ -0,0 +1,54 @@
+var fs = require('fs');
+var es = require('event-stream');
+var Q = require('q');
+
+module.exports.parse = function(filePath) {
+    var deferred = Q.defer();
+    var stream;
+
+    var transactionData = [];
+    var formattedData = [];
+
+    if (!filePath) {
+        throw 'No filePath provided to dataParser.'
+    }
+
+    stream = fs.createReadStream(filePath)
+        .pipe(es.split())
+        .pipe(es.mapSync(function(line) {
+            // Pause the read stream.
+            stream.pause();
+
+            (function() {
+                // Ignore blank lines.
+                if (!line) {
+                    stream.resume();
+                    return;
+                }
+
+                transactionData = line.split(',');
+
+                formattedData.push({
+                    groups: {
+                        testing: JSON.parse(transactionData[0].replace(/;/g, ',')),
+                        validation: JSON.parse(transactionData[1].replace(/;/g, ','))
+                    },
+                    timestamp: new Date(transactionData[2]),
+                    volume: 0,
+                    open: parseFloat(transactionData[3]),
+                    high: parseFloat(transactionData[4]),
+                    low: parseFloat(transactionData[5]),
+                    close: parseFloat(transactionData[6])
+                });
+
+                // Resume the read stream.
+                stream.resume();
+            })();
+        }));
+
+    stream.on('close', function() {
+        deferred.resolve(formattedData);
+    });
+
+    return deferred.promise;
+};

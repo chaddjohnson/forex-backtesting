8e5e9a537e0f2007cd1c16ec0455733cf384fcb2
Chad Johnson <chad.d.johnson@gmail.com>
2016-06-09 01:17:32 -0500

    Using 2x dataChunkSize when Real == float.

--- lib/optimizers/optimizer.cu
+++ lib/optimizers/optimizer.cu
@@ -401,7 +401,7 @@ void Optimizer::optimize(Real investment, Real profitability) {
     int dataPropertyCount = getDataPropertyCount();
     int dataPointCount;
     int configurationCount = configurations.size();
-    int dataChunkSize = 500000;
+    int dataChunkSize = sizeof(Real) == sizeof(double) ? 500000 : 1000000;
     int dataOffset = 0;
     int chunkNumber = 1;
     int dataPointIndex = 0;

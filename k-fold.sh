e72327b4ffc2a47da9d6bf57d2993d3af0cf778b
Chad Johnson <chad.d.johnson@gmail.com>
2016-06-12 23:32:38 -0500

    k-fold.sh can now receive one or more symbols.

--- k-fold.sh
+++ k-fold.sh
@@ -1,52 +1,54 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-  echo "Symbol must be provided."
+if [ "$#" -lt 1 ]; then
+  echo "One or more symbols must be provided."
   exit 1
 fi
 
-# Prepare data.
-./bin/prepareData --symbol $1 --parser oanda --optimizer reversals --file /home/chad/development/desktop/forex-backtesting/data/oanda/k-fold/combined/$1.csv
+for symbol in $@; do
+    # Prepare data.
+    # ./bin/prepareData --symbol $symbol --parser oanda --optimizer reversals --file /home/chad/development/desktop/forex-backtesting/data/oanda/k-fold/combined/$symbol.csv
 
-# Perform k-fold testing for group 1.
-echo; echo "k-fold group 1 testing..."; time ./bin/optimize --symbol $1 --type testing --group 1 --optimizer reversals --investment 1000 --profitability 0.76
-echo; echo "k-fold group 1 validation..."; time ./bin/optimize --symbol $1 --type validation --group 1 --optimizer reversals --investment 1000 --profitability 0.76
+    # Perform k-fold testing for group 1.
+    echo; echo "$symbol k-fold group 1 testing..."; time ./bin/optimize --symbol $symbol --type testing --group 1 --optimizer reversals --investment 1000 --profitability 0.76
+    echo; echo "$symbol k-fold group 1 validation..."; time ./bin/optimize --symbol $symbol --type validation --group 1 --optimizer reversals --investment 1000 --profitability 0.76
 
-# Perform k-fold testing for group 2.
-echo; echo "k-fold group 2 testing..."; time ./bin/optimize --symbol $1 --type testing --group 2 --optimizer reversals --investment 1000 --profitability 0.76
-echo; echo "k-fold group 2 validation..."; time ./bin/optimize --symbol $1 --type validation --group 2 --optimizer reversals --investment 1000 --profitability 0.76
+    # Perform k-fold testing for group 2.
+    echo; echo "$symbol k-fold group 2 testing..."; time ./bin/optimize --symbol $symbol --type testing --group 2 --optimizer reversals --investment 1000 --profitability 0.76
+    echo; echo "$symbol k-fold group 2 validation..."; time ./bin/optimize --symbol $symbol --type validation --group 2 --optimizer reversals --investment 1000 --profitability 0.76
 
-# Perform k-fold testing for group 3.
-echo; echo "k-fold group 3 testing..."; time ./bin/optimize --symbol $1 --type testing --group 3 --optimizer reversals --investment 1000 --profitability 0.76
-echo; echo "k-fold group 3 validation..."; time ./bin/optimize --symbol $1 --type validation --group 3 --optimizer reversals --investment 1000 --profitability 0.76
+    # Perform k-fold testing for group 3.
+    echo; echo "$symbol k-fold group 3 testing..."; time ./bin/optimize --symbol $symbol --type testing --group 3 --optimizer reversals --investment 1000 --profitability 0.76
+    echo; echo "$symbol k-fold group 3 validation..."; time ./bin/optimize --symbol $symbol --type validation --group 3 --optimizer reversals --investment 1000 --profitability 0.76
 
-# Perform k-fold testing for group 4.
-echo; echo "k-fold group 4 testing..."; time ./bin/optimize --symbol $1 --type testing --group 4 --optimizer reversals --investment 1000 --profitability 0.76
-echo; echo "k-fold group 4 validation..."; time ./bin/optimize --symbol $1 --type validation --group 4 --optimizer reversals --investment 1000 --profitability 0.76
+    # Perform k-fold testing for group 4.
+    echo; echo "$symbol k-fold group 4 testing..."; time ./bin/optimize --symbol $symbol --type testing --group 4 --optimizer reversals --investment 1000 --profitability 0.76
+    echo; echo "$symbol k-fold group 4 validation..."; time ./bin/optimize --symbol $symbol --type validation --group 4 --optimizer reversals --investment 1000 --profitability 0.76
 
-# Perform k-fold testing for group 5.
-echo; echo "k-fold group 5 testing..."; time ./bin/optimize --symbol $1 --type testing --group 5 --optimizer reversals --investment 1000 --profitability 0.76
-echo; echo "k-fold group 5 validation..."; time ./bin/optimize --symbol $1 --type validation --group 5 --optimizer reversals --investment 1000 --profitability 0.76
+    # Perform k-fold testing for group 5.
+    echo; echo "$symbol k-fold group 5 testing..."; time ./bin/optimize --symbol $symbol --type testing --group 5 --optimizer reversals --investment 1000 --profitability 0.76
+    echo; echo "$symbol k-fold group 5 validation..."; time ./bin/optimize --symbol $symbol --type validation --group 5 --optimizer reversals --investment 1000 --profitability 0.76
 
-# Perform k-fold testing for group 6.
-echo; echo "k-fold group 6 testing..."; time ./bin/optimize --symbol $1 --type testing --group 6 --optimizer reversals --investment 1000 --profitability 0.76
-echo; echo "k-fold group 6 validation..."; time ./bin/optimize --symbol $1 --type validation --group 6 --optimizer reversals --investment 1000 --profitability 0.76
+    # Perform k-fold testing for group 6.
+    echo; echo "$symbol k-fold group 6 testing..."; time ./bin/optimize --symbol $symbol --type testing --group 6 --optimizer reversals --investment 1000 --profitability 0.76
+    echo; echo "$symbol k-fold group 6 validation..."; time ./bin/optimize --symbol $symbol --type validation --group 6 --optimizer reversals --investment 1000 --profitability 0.76
 
-# Perform k-fold testing for group 7.
-echo; echo "k-fold group 7 testing..."; time ./bin/optimize --symbol $1 --type testing --group 7 --optimizer reversals --investment 1000 --profitability 0.76
-echo; echo "k-fold group 7 validation..."; time ./bin/optimize --symbol $1 --type validation --group 7 --optimizer reversals --investment 1000 --profitability 0.76
+    # Perform k-fold testing for group 7.
+    echo; echo "$symbol k-fold group 7 testing..."; time ./bin/optimize --symbol $symbol --type testing --group 7 --optimizer reversals --investment 1000 --profitability 0.76
+    echo; echo "$symbol k-fold group 7 validation..."; time ./bin/optimize --symbol $symbol --type validation --group 7 --optimizer reversals --investment 1000 --profitability 0.76
 
-# Perform k-fold testing for group 8.
-echo; echo "k-fold group 8 testing..."; time ./bin/optimize --symbol $1 --type testing --group 8 --optimizer reversals --investment 1000 --profitability 0.76
-echo; echo "k-fold group 8 validation..."; time ./bin/optimize --symbol $1 --type validation --group 8 --optimizer reversals --investment 1000 --profitability 0.76
+    # Perform k-fold testing for group 8.
+    echo; echo "$symbol k-fold group 8 testing..."; time ./bin/optimize --symbol $symbol --type testing --group 8 --optimizer reversals --investment 1000 --profitability 0.76
+    echo; echo "$symbol k-fold group 8 validation..."; time ./bin/optimize --symbol $symbol --type validation --group 8 --optimizer reversals --investment 1000 --profitability 0.76
 
-# Perform k-fold testing for group 9.
-echo; echo "k-fold group 9 testing..."; time ./bin/optimize --symbol $1 --type testing --group 9 --optimizer reversals --investment 1000 --profitability 0.76
-echo; echo "k-fold group 9 validation..."; time ./bin/optimize --symbol $1 --type validation --group 9 --optimizer reversals --investment 1000 --profitability 0.76
+    # Perform k-fold testing for group 9.
+    echo; echo "$symbol k-fold group 9 testing..."; time ./bin/optimize --symbol $symbol --type testing --group 9 --optimizer reversals --investment 1000 --profitability 0.76
+    echo; echo "$symbol k-fold group 9 validation..."; time ./bin/optimize --symbol $symbol --type validation --group 9 --optimizer reversals --investment 1000 --profitability 0.76
 
-# Perform k-fold testing for group 10.
-echo; echo "k-fold group 10 testing..."; time ./bin/optimize --symbol $1 --type testing --group 10 --optimizer reversals --investment 1000 --profitability 0.76
-echo; echo "k-fold group 10 validation..."; time ./bin/optimize --symbol $1 --type validation --group 10 --optimizer reversals --investment 1000 --profitability 0.76
+    # Perform k-fold testing for group 10.
+    echo; echo "$symbol k-fold group 10 testing..."; time ./bin/optimize --symbol $symbol --type testing --group 10 --optimizer reversals --investment 1000 --profitability 0.76
+    echo; echo "$symbol k-fold group 10 validation..."; time ./bin/optimize --symbol $symbol --type validation --group 10 --optimizer reversals --investment 1000 --profitability 0.76
 
-# Perform k-fold averaging.
-# TODO
+    # Perform k-fold averaging.
+    # TODO
+done
